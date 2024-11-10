f5f41576df9e6bf71e8834057cf0a0921aa81372
Benny Mendoza <bennybc9387@gmail.com>
2024-11-10 13:56:22 -0700

    added description to component

---
+++ src/components/descriptionAndCaptions.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import LazyImage from "./LazyImage";
+import { data } from "../constant/data";  // Import your data array here
+import "./LazyImage.css";
+
+const ImageGallery = () => {
+  return (
+    <div className="image-gallery">
+      {data.map((item, index) => (
+        <LazyImage
+          key={index}
+          src={item.src}
+          alt={item.caption}
+          caption={item.caption}
+          description={item.description}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ImageGallery;
